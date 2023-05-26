0340bc45abe350de1a6439649c9a4ef0e37862b1
pavan <pavanrajasekhar@gmail.com>
2023-05-26 10:21:19 -0400

    Test fail
    Failing the first test

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -8,27 +8,33 @@
 int main(int argc, char *argv[])
 {
     void filecopy(FILE *, FILE *);
-  
-    FILE *fp = fopen("wcat.c", "r");
-    if (fp == NULL)
-    {
-        printf("wcat: cannot open file \n");
-        exit(1);
-    }
-    else
-    {
-        filecopy(fp, stdout);
+    void fileerr(char *);
+
+    FILE *fp ;
+
+    if(argc == 1)
+        filecopy(stdin, stdout);
+    else 
+    while (-- argc > 0){
+        if((fp = fopen(*++argv, "r")) == NULL){
+            fileerr(*argv);
+        } else {
+            filecopy(fp, stdout);
+            fclose(fp);
+        }
     }
     return 0;
 }
 
-
 // Method to print the file
+void fileerr(char *filename){
+        printf("wcat: cannot open file %s \n", *filename);
+        exit(1);
+}
 
 void filecopy(FILE *input, FILE *output){
     int c; 
     while(( c= getc(input)) != EOF)
         putc(c, output);
-
 }
 
