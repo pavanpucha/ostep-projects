e0ca6f304bc39ad9a34c120b02f4c51cadde2630
pavan <pavanrajasekhar@gmail.com>
2023-05-26 11:08:41 -0400

    WCAT complete
    WCAT passed all tests. GTG

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,5 +1,5 @@
 /*
-    The C programming Language, 2nd ed.  Section 7.5
+    The C programming Language, 2nd ed.  Section 7.5, 7.6
 */
 
 #include <stdio.h>
@@ -8,28 +8,29 @@
 int main(int argc, char *argv[])
 {
     void filecopy(FILE *, FILE *);
-    void fileerr(char *);
 
     FILE *fp ;
+    char *prog = argv[0]; /* program name for the errors*/
 
-    if(argc == 1)
-        filecopy(stdin, stdout);
+    if(argc == 1) {
+        return 0;
+    }
     else 
     while (-- argc > 0){
         if((fp = fopen(*++argv, "r")) == NULL){
-            fileerr(*argv);
+        printf("wcat: cannot open file\n");
+        return 1;
         } else {
             filecopy(fp, stdout);
             fclose(fp);
         }
     }
-    return 0;
-}
 
-// Method to print the file
-void fileerr(char *filename){
-        printf("wcat: cannot open file %s \n", *filename);
-        exit(1);
+    if(ferror(stdout)){
+        fprintf(stderr, "%s error writing to stdout \n", prog);
+      return 1;
+    }
+    return 0;
 }
 
 void filecopy(FILE *input, FILE *output){
